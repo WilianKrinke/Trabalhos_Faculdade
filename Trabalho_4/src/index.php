a966463ef7d7ec24146b0c7857f571dfae942587
Wilian Krinke <krinkewilian@gmail.com>
2021-11-16 20:42:00 -0300

    bug

--- Trabalho_4/src/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require __DIR__ . '/vendor/autoload.php';
-include('./utils/environments.php');
-
-$classeTeste = new Environments();
-$classeTeste->load(__DIR__);
-
-
-print_r(getenv('PASS_SQL'));
