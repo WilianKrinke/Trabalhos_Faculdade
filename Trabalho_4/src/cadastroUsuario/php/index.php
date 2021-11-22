a6af248b8de23b424344e4eda8f1ce016212618a
Wilian Krinke <krinkewilian@gmail.com>
2021-11-22 20:02:37 -0300

    ajustes

--- Trabalho_4/src/cadastroUsuario/php/index.php
+++ Trabalho_4/src/cadastroUsuario/php/index.php
@@ -16,7 +16,7 @@ if ((isset($_SESSION['userName']) == true) and (isset($_SESSION['isAdm']) == tru
     <link rel="stylesheet" href="../../utils//global_css//globalCss.css">
     <link rel="stylesheet" href="../../utils//global_css//buttons.css">
     <link rel="stylesheet" href="../../utils//global_css//links.css">
-    <link rel="stylesheet" href="../css//cadastroUsuario.css">
+    <link rel="stylesheet" href="../css/cadastroUsuario.css">
     <title>Programação Web - PUC - Pr</title>
 </head>
 
