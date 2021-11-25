49c966ff4757b7ba399b7a798b75b2704b866a0d
Wilian Krinke <krinkewilian@gmail.com>
2021-11-25 19:45:20 -0300

    metodos de ordenação - merge sort e selection sort

--- Trabalho_5/Métodos de Ordenação/src/metodos_de_ordenacao/Merge_Sort.java
+++ Trabalho_5/Métodos de Ordenação/src/metodos_de_ordenacao/Merge_Sort.java
@@ -1,10 +1,83 @@
 package metodos_de_ordenacao;
 
 public class Merge_Sort {
+	public static void mergeSort(int[] vetor) {
+		
+		int inputlength = vetor.length;
+		
+		if (inputlength < 2) {
+			return;
+		}
+		
+		int indexMeio = inputlength / 2;
+		int[] vetorEsquerda = new int[indexMeio];
+		int[] vetorDireita = new int[inputlength - indexMeio];
+		
+		for (int i = 0; i < indexMeio; i++) {
+			vetorEsquerda[i] = vetor[i];
+		}
+		
+		for (int i = indexMeio; i < inputlength; i++) {
+			vetorDireita[i - indexMeio] = vetor[i];
+		}
+		
+		mergeSort(vetorEsquerda);
+		mergeSort(vetorDireita);
+		
+		//intercalação
+		merge(vetor, vetorEsquerda, vetorDireita);
+		
+		
+	}
+	
+	public static void merge(int[] vetorOriginal, int[] vetorEsquerda, int[] vetorDireita) {
+		
+		int ladoEsquerdoTamanho = vetorEsquerda.length;
+		int ladoDireitoTamanho = vetorDireita.length;
+		
+		int i = 0, j = 0, k = 0;
+
+		while (i < ladoEsquerdoTamanho && j < ladoDireitoTamanho) {
+			if (vetorEsquerda[i] <= vetorDireita[j]) {
+				vetorOriginal[k] = vetorEsquerda[i];
+				i++;
+			} else {
+				vetorOriginal[k] = vetorDireita[j];
+				j++;
+			}
+			k++;
+		}
+		
+		while (i < ladoEsquerdoTamanho) {
+			vetorOriginal[k] = vetorEsquerda[i];
+			i++;
+			k++;
+		}
+		
+		while (j < ladoDireitoTamanho) {
+			vetorOriginal[k] = vetorDireita[j];
+			j++;
+			k++;
+		}
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int vetor[] = {915,951,5,1,95,15,19,519,51,95,195,1,951,6,12,16,21,6};
+		
+		System.out.println("Desordenado");
+		for (int i = 0; i < vetor.length; i++) {
+			System.out.print(vetor[i] + " ");
+		}
+		
+		System.out.println("\n");
+		System.out.println("Ordenado");
+		
+		mergeSort(vetor);
+		
+		
+		for (int i = 0; i < vetor.length; i++) {
+			System.out.print(vetor[i] + " ");
+		}
 	}
 
 }
--- Trabalho_5/Métodos de Ordenação/src/metodos_de_ordenacao/Selection_Sort.java
+++ Trabalho_5/Métodos de Ordenação/src/metodos_de_ordenacao/Selection_Sort.java
@@ -3,7 +3,28 @@ package metodos_de_ordenacao;
 public class Selection_Sort {
 
 	public static void main(String[] args) {
+		int vetor[] = {18,4,2,6,8,10,16,18,14};
 		
+		int posicao_menor;
+		int aux; 
+		
+		for (int i = 0; i < vetor.length; i++) {
+			posicao_menor = i;
+			
+			for (int j = i + 1; j < vetor.length; j++) {
+				if (vetor[j] < vetor[posicao_menor]) {
+					posicao_menor = j;
+				}
+			}
+			
+			aux = vetor[posicao_menor];
+			vetor[posicao_menor] = vetor[i];
+			vetor[i] = aux;
+		}
+		
+		
+		for (int i = 0; i < vetor.length; i++) {
+			System.out.print(vetor[i] + " ");
+		}
 	}
-
 }
