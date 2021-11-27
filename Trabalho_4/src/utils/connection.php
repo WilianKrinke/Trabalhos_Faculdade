d0770df432e0a3734effde647d491264163b16ad
Wilian Krinke <66577033+WilianKrinke@users.noreply.github.com>
2021-11-27 00:58:47 -0300

    Update connection.php

--- Trabalho_4/src/utils/connection.php
+++ Trabalho_4/src/utils/connection.php
@@ -1,9 +1,9 @@
 <?php
-$host = "localhost";
-$user = "root";
-$passSql = "88653730";
-$database = "fundamentosweb";
-$port = "3306";
+$host = "";
+$user = "";
+$passSql = "";
+$database = "";
+$port = "";
 
 try {
     $connection = new PDO("mysql:host=$host;port=$port;dbname=" . $database, $user, $passSql);
