08dc6436b11e61c74b9bfa7be2b0cceda32617c9
Wilian Krinke <krinkewilian@gmail.com>
2021-11-27 21:34:16 -0300

    correção de bubble sort

--- Trabalho_5/Métodos de Ordenação/src/metodos_de_ordenacao/Bubble_Sort.java
+++ Trabalho_5/Métodos de Ordenação/src/metodos_de_ordenacao/Bubble_Sort.java
@@ -5,18 +5,28 @@ public class Bubble_Sort {
 	public static void main(String[] args) {
 		int vetor[] = {3,6,8,1,4,9,0};
 		int aux;
+		boolean control;
 		
-		for (int i = 0; i < vetor.length; i++) {			
+		for (int i = 0; i < vetor.length; i++) {
+			control = true;
 			
-			for (int j = i + 1; j < vetor.length; j++) {
-				if (vetor[i] > vetor[j]) {
+			for (int j = 0; j < (vetor.length - 1); j++) {
+				
+				if (vetor[j] > vetor[j + 1]) {
 					aux = vetor[j];
-					vetor[j] = vetor[i];
-					vetor[i] = aux;					
+					vetor[j] = vetor[j + 1];
+					vetor[j + 1] = aux;
+					control = false;
 				}				
-			}			
+			}
+			
+			if (control) {
+				break; 
+			}
 		}
 		
+		
+		System.out.println("Vetor Ordenado");
 		for (int i = 0; i < vetor.length; i++) {
 			System.out.print(vetor[i] + " ");
 		}
