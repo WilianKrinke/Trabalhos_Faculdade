b17a1d4d321eec58fd6bc30744a8b304421f917d
Wilian Krinke <krinkewilian@gmail.com>
2022-09-07 17:00:04 -0300

    aperfeiçoando codigo

--- Trabalho_7/device.py
+++ Trabalho_7/device.py
@@ -1,19 +1,27 @@
 import paho.mqtt.client as mqtt
 import time
 
-user = ""
-password = ""
+user: str = ""
+password: str = ""
 
-clientId = "stringqualquer"
-server = "broker.mqttdashboard.com"
-port = 1883
+clientId: str = "stringqualquer"
+server: str = "broker.mqttdashboard.com"
+port: int = 1883
 
 client = mqtt.Client(clientId)
 client.username_pw_set(user, password)
 
+temperatura: int = 22.3
+humidade: int = 60
+
 client.connect(server, port)
 
-client.publish("testeTopic/iot/willk/temperatura", 22.3)
-time.sleep(1)
+controle: int = 1
+while controle <= 6:
+    client.publish("testeTopic/iot/willk/temperatura", temperatura)
+    client.publish("testeTopic/iot/willk/humidade", humidade)
+    print(f"Enviando {controle}* temperatura: {temperatura} e humidade: {humidade}")
+    time.sleep(1)
+    controle += 1
 
 client.disconnect()
