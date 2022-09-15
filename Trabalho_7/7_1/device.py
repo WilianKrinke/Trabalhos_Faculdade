526d586aeb4962ef1f66b6d42ab3d58fa6baa853
Wilian Krinke <krinkewilian@gmail.com>
2022-09-15 15:09:54 -0300

    inserindo nova estrutura

---
+++ Trabalho_7/7_1/device.py
@@ -0,0 +1,19 @@
+import paho.mqtt.client as mqtt
+from hal import recepcaoMensagemAquecedor
+from mqttConfig import user, password, clientId, server, port
+from sendMessages import sendMessages
+
+# conexão inicial
+client = mqtt.Client(clientId)
+client.username_pw_set(user, password)
+client.connect(server, port)
+
+
+client.on_message = recepcaoMensagemAquecedor
+client.subscribe(f"v1/{user}/things/{clientId}/cmd/2")
+# Se inserir curingas, atente-se para filtragem do tópico
+client.loop_start()
+
+sendMessages(client)
+
+client.disconnect()
