45a94e78a06e3a4bd9292d342c17356fb98112a6
Wilian Krinke <krinkewilian@gmail.com>
2022-10-01 20:44:52 -0300

    inserindo codigos

---
+++ Trabalho_8/Atividade_Diagnostica_SegurancaDaInformacao/listaFilmes.py
@@ -0,0 +1,8 @@
+listaFilmes: list = []
+
+for item in range(5):
+    filme: str = input("Insira o nome de um filme: ")
+    listaFilmes.append(filme)
+
+
+print(listaFilmes)
---
+++ Trabalho_8/Atividade_Diagnostica_SegurancaDaInformacao/materialEscolar.py
@@ -0,0 +1,24 @@
+
+
+listaMaterial: list = []
+dictMaterial2: dict = {}
+
+for item in range(4):
+    dictMaterial1: dict = {}
+
+    nomeMaterial: str = input(f"Insira o nome do {item + 1}* material: ")
+    valorMaterial: float = float(
+        input(f"Insira o valor do {item + 1}* produto: "))
+
+    dictMaterial1["NomeMaterial"]: str = nomeMaterial
+    dictMaterial1["ValorMaterial"]: float = valorMaterial
+
+    listaMaterial.append(dictMaterial1)
+
+
+segundoDicionario: dict = listaMaterial.pop(1)
+
+listaMaterial[0]["NomeMaterial"]: str = segundoDicionario["NomeMaterial"]
+listaMaterial[0]["ValorMaterial"]: float = segundoDicionario["ValorMaterial"]
+
+print(listaMaterial)
