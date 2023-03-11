d75309f1d64d8a32d6b5e2b46f930cef1c273e75
Wilian Krinke <krinkewilian@gmail.com>
2023-03-11 17:35:53 -0300

    inserindo class component

--- Trabalho_9/calculadora/src/components/ecra_component/ecra.jsx
+++ Trabalho_9/calculadora/src/components/ecra_component/ecra.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import "./ecra.css"
 
-const Ecra = () => {
-    return (
-        <div className='ecra'>
-            <p className='display'>0.75</p>
-        </div>
-    );
+
+class Ecra extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            num: 0.75,
+        }
+    }
+
+
+    render() {
+        return (
+            <div className='ecra'>
+                <p className='display'>{this.state.num}</p>
+            </div>
+        );
+    }
 }
 
 export default Ecra;
