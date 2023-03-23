527fd3e7c7e3f0e0580fd0d892883cce69c23d75
Wilian Krinke <krinkewilian@gmail.com>
2023-03-22 23:55:26 -0300

    inserindo tags html

---
+++ Trabalho_9/teste-de-rotas/src/pages/Home/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Title from '../../components/Title_component'
+import './home.css'
+
+const Index = () => {
+    return (
+        <>
+            <header className='header_class'>
+                <Title title={'Home'}/>
+            </header>
+            <main>
+            
+            </main>
+            <footer>
+                
+            </footer>                  
+        </>
+    );
+}
+
+export default Index;
--- Trabalho_9/teste-de-rotas/src/routes/index.js
+++ Trabalho_9/teste-de-rotas/src/routes/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from '../pages/Home'
 
 const index = () => {
     return (
         <BrowserRouter>
             <Routes>
-
+                <Route exact path='/' Component={Home}/>
             </Routes>            
         </BrowserRouter>
     );
